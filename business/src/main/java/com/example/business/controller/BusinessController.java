d5c9a7332484e7c1370c2e2b5729e2ca063b2539
Phani Gopaluni <gopaluniphani@gmail.com>
2021-01-23 11:43:32 +0530

    Added controller for basic functionality

--- business/src/main/java/com/example/business/controller/BusinessController.java
+++ business/src/main/java/com/example/business/controller/BusinessController.java
@@ -1,14 +1,48 @@
 package com.example.business.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.business.models.Business;
+import com.example.business.models.Posts;
+import com.example.business.models.Response;
+import com.example.business.services.BusinessService;
+import com.example.business.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/pagebook/api/business/")
 public class BusinessController {
-    @GetMapping(value = "/sample")
-    public String returnSample() {
-        return "sample message";
+
+    @Autowired
+    BusinessService businessService;
+
+    @Autowired
+    PostsService postsService;
+
+    @PostMapping(value = "/")
+    public Response createBusiness(@RequestBody Business business) {
+        Business newBusiness = businessService.save(business);
+        return new Response(true, "", newBusiness);
+    }
+
+    @GetMapping(value = "/{id}")
+    public Response retrieveBusinessDetails(@PathVariable("id") String businessId) {
+        Optional<Business> business = businessService.findById(businessId);
+        Response response = new Response();
+        if(business.isPresent()) {
+            response.setStatus(true);
+            response.setBody(business.get());
+        } else {
+            response.setStatus(false);
+            response.setErrorMessage("Cannot find any business with the given ID");
+        }
+        return  response;
+    }
+
+    @GetMapping(value = "/posts/{id}")
+    public Response getPostsByBusinessId(@PathVariable("id") String businessId) {
+        Posts posts = postsService.findByBusinessId(businessId);
+        return new Response(true, "", posts);
     }
 }
