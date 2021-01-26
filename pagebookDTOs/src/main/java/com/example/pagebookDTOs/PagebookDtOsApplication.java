84407a3cb86590b30eaff1b79ca811c7832886f8
AkshayMehta2301 <70093798+AkshayMehta2301@users.noreply.github.com>
2021-01-26 09:22:58 +0530

    initial commit for dtos

---
+++ pagebookDTOs/src/main/java/com/example/pagebookDTOs/PagebookDtOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagebookDTOs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagebookDtOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagebookDtOsApplication.class, args);
+	}
+
+}
